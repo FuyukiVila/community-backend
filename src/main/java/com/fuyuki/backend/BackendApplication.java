3557ef88cbaf18d6cdc117b2b9fc7cad76ba3890
FuyukiVila <1642421711@qq.com>
2024-11-05 13:58:03 +0800

    用户注册和登录

--- src/main/java/com/fuyuki/backend/BackendApplication.java
+++ src/main/java/com/fuyuki/backend/BackendApplication.java
@@ -3,10 +3,12 @@ package com.fuyuki.backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @MapperScan("com.fuyuki.backend.mapper")
+@ComponentScan("com.fuyuki.backend.model.dto")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
---
+++ src/main/java/com/fuyuki/backend/mapper/UmsUserMapper.java
@@ -0,0 +1,14 @@
+package com.fuyuki.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fuyuki.backend.model.entity.UmsUser;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 用户
+ *
+ */
+@Repository
+public interface UmsUserMapper extends BaseMapper<UmsUser> {
+
+}
