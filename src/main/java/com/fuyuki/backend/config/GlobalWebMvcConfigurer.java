b3e9676dce587993f25ea1f6b0774ea2dedaf702
FuyukiVila <1642421711@qq.com>
2024-11-13 21:52:02 +0800

    update

--- src/main/java/com/fuyuki/backend/config/GlobalWebMvcConfigurer.java
+++ src/main/java/com/fuyuki/backend/config/GlobalWebMvcConfigurer.java
@@ -18,7 +18,7 @@ public class GlobalWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 明确指定允许的本地源
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080") // 明确指定允许的本地源
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
@@ -29,7 +29,8 @@ public class GlobalWebMvcConfigurer implements WebMvcConfigurer {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         // 明确指定允许的本地源
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://127.0.0.1:8080");
         // 允许跨越发送cookie
         config.setAllowCredentials(true);
         // 放行全部原始头信息
