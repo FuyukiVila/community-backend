c7e3b04be05d9c98435dcd5408b4d58ae8f76a72
FuyukiVila <1642421711@qq.com>
2024-11-26 15:02:48 +0800

    update

--- src/main/java/com/fuyuki/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fuyuki/backend/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.fuyuki.backend.common.exception;
 
 import com.fuyuki.backend.common.api.ApiResult;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResult<?>> handleRuntimeException(RuntimeException ex) {
-        if (ex.getMessage().contains("Token expired")) {
-            // 处理 Token 过期的情况
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResult.unauthorized(null));
-        } else {
-            // 处理其他运行时异常
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResult.failed(ex.getMessage()));
-        }
+        return ResponseEntity.status(404).body(ApiResult.failed(ex.getMessage()));
     }
 }
