337e190a3a65b524383673131d84f4a1a945dcd7
FuyukiVila <1642421711@qq.com>
2024-12-24 21:46:46 +0800

    delete: url

--- src/main/java/com/fuyuki/backend/config/GlobalWebMvcConfigurer.java
+++ src/main/java/com/fuyuki/backend/config/GlobalWebMvcConfigurer.java
@@ -19,7 +19,7 @@ public class GlobalWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:3000", "http://127.0.0.1:3000", "https://adapted-feline-suited.ngrok-free.app") // 明确指定允许的本地源
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:3000", "http://127.0.0.1:3000") // 明确指定允许的本地源
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
@@ -34,7 +34,6 @@ public class GlobalWebMvcConfigurer implements WebMvcConfigurer {
         config.addAllowedOrigin("http://127.0.0.1:8080");
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("http://127.0.0.1:3000");
-        config.addAllowedOrigin("https://adapted-feline-suited.ngrok-free.app");
         // 允许跨越发送cookie
         config.setAllowCredentials(true);
         // 放行全部原始头信息
